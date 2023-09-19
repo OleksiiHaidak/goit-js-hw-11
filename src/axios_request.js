f0016136a3242b261b49ae35581e89edd9050477
OleksiiHaidak <xalexey.g@gmail.com>
2023-09-19 12:16:20 +0300

    update js

--- src/axios_request.js
+++ src/axios_request.js
@@ -2,15 +2,12 @@ import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-
-
 const pixabayApiKey = "39445880-54b6798b916331fa60d6cbc62";
 const url = "https://pixabay.com/api/";
 
 export async function getImages(query, page, perPage) {
   try {
     const response = await axios.get(`${url}?key=${pixabayApiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
-    totalHits = response.data.totalHits;
     return response.data;
   } catch (error) {
     Notify.failure('Sorry, there are no images matching your search query. Please try again.');
