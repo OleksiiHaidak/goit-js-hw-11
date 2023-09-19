6004164f2ba12eaafcae6b2eaa9eb63b67669ce7
OleksiiHaidak <xalexey.g@gmail.com>
2023-09-19 10:07:31 +0300

    Update js files

--- src/index.js
+++ src/index.js
@@ -1,79 +1,74 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import axios from 'axios';
+import { getImages } from "./axios_request";
+
 
 const searchForm = document.querySelector(".search-form");
 const searchInput = document.querySelector("input[name='searchQuery']");
-const searchBtn = document.querySelector("button[type='submit']");
 const gallery = document.querySelector(".gallery");
 const target = document.querySelector(".js-guard");
 
-const pixabayApiKey = '39445880-54b6798b916331fa60d6cbc62';
-const url = 'https://pixabay.com/api/';
+
+let lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
 const perPage = 40;
 let page = 1;
 let totalHits = 0;
+
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const query = searchInput.value.trim();
   page = 1;
-  getImages(query);
+  getImages(query, page, perPage).then(createMarkup);
   gallery.innerHTML = "";
   observer.unobserve(target);
 });
 
 
-async function getImages(query) {
-  try {
-    const response = await axios.get(`${url}?key=${pixabayApiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
-    
-    totalHits = response.data.totalHits;
+async function createMarkup(response) {
 
-    if (totalHits === 0 && !error) {
-      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-};
-    
-    if (page === 1) {
-      Notify.success(`Hooray! We found ${totalHits} images.`);
-};
-    createMarkup(response);
-    
-  } catch (error) {
+  if (response.totalHits === 0) {
     Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    return;
   };
-};
 
+  if (page === 1) {
+      Notify.success(`Hooray! We found ${response.totalHits} images.`);
+  };
 
+    if (response.hits.length === 0) {
+    observer.unobserve(target);
+    return;
+  };
 
-function createMarkup(response) {
-const imagesMarkup = response.data.hits.map((image) => `
-      <div class="photo-card">
-        <a href="${image.largeImageURL}">
-          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
-        </a>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes:</b> ${image.likes}
-          </p>
-          <p class="info-item">
-            <b>Views:</b> ${image.views}
-          </p>
-          <p class="info-item">
-            <b>Comments:</b> ${image.comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads:</b> ${image.downloads}
-          </p>
-        </div>
+  const imagesMarkup = response.hits.map((image) => `
+    <div class="photo-card">
+      <a href="${image.largeImageURL}">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes:</b> ${image.likes}
+        </p>
+        <p class="info-item">
+          <b>Views:</b> ${image.views}
+        </p>
+        <p class="info-item">
+          <b>Comments:</b> ${image.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads:</b> ${image.downloads}
+        </p>
       </div>
-    `).join('');
+    </div>
+  `).join("");
 
-  gallery.insertAdjacentHTML('beforeend', imagesMarkup);
-  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+  gallery.insertAdjacentHTML("beforeend", imagesMarkup);
+  lightbox.refresh();
   observer.observe(target);
- };
+};
 
 
 
@@ -89,22 +84,20 @@ function onLoad(entries, observer) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       page++;
-      getImages(searchInput.value.trim());
-      SimpleLightbox.refresh();
+      getImages(searchInput.value.trim(), page, perPage).then(createMarkup);
 
+      const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
 
-  const { height: cardHeight } = document
-      .querySelector(".gallery")
-      .firstElementChild.getBoundingClientRect();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+      });
 
-  window.scrollBy({
-       top: cardHeight * 2,
-       behavior: "smooth",
-});
-      
-      if (page * perPage >= totalHits) {
+      if (page > Math.ceil(totalHits / perPage)) {
         observer.unobserve(target);
       };
     };
-   });
+  });
 };
